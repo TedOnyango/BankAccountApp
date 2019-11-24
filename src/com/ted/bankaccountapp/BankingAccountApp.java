9c72df112e41bfbbb70d1e0277cc5b1e988283ac
TedOnyango <teddonyango@gmail.com>
2019-11-24 11:22:46 +0300

    Determining which account to open

--- src/com/ted/bankaccountapp/BankingAccountApp.java
+++ src/com/ted/bankaccountapp/BankingAccountApp.java
@@ -1,21 +1,15 @@
 package com.ted.bankaccountapp;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class BankingAccountApp {
 
     public static void main(String[] args) {
-        String file = "/home/ted/Videos/NewBankAccounts.csv";
-//        Checking checkingacc1 = new Checking("Ted Onyi", "25654", 100);
-//        Savings savingsacc1 = new Savings("Chris Otieno", "35487", 1000);
-//
-//        savingsacc1.compound();
-//
-//        checkingacc1.showInfo();
-//        System.out.println("***********************");
-//        savingsacc1.showInfo();
+        List<Account> accounts = new LinkedList<Account>();
 
         // Read CSV file and create new accounts based on the data
+        String file = "/home/ted/Videos/NewBankAccounts.csv";
         List<String []> newAccountHolders = utilities.CSV.read(file);
         for(String [] newAccountHolder: newAccountHolders){
             String name = newAccountHolder[0];
@@ -23,14 +17,18 @@ public class BankingAccountApp {
             String accountType = newAccountHolder[2];
             double deposit = Double.parseDouble(newAccountHolder[3]);
             if (accountType.equals("Savings")){
-                System.out.println("Open Savings Account");
+                accounts.add(new Savings(name, sSN, deposit));
             }
             else if (accountType.equals("Checking")){
-                System.out.println("Open Checking Account");
+                accounts.add(new Checking(name, sSN, deposit));
             }
             else {
                 System.out.println("Error Reading Account type");
             }
+        }
+        for (Account acc : accounts){
+            acc.showInfo();
+
         }
     }
 }
